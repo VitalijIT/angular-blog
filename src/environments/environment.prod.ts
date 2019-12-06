85b60ef8ef1f3801be6172ed29c12f0b70db63d2
Vitalij <vitalibodnar@gmail.com>
2019-12-06 09:05:04 +0100

    1 final version

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,3 +1,7 @@
-export const environment = {
-  production: true
+import {Environment} from './interface';
+
+export const environment: Environment = {
+  production: true,
+  apiKey: 'AIzaSyAnBKuubIFIrAyp-iZu1lpI2bHg6jSjqas',
+  fbDbUrl: 'https://angular-blog-9f7f0.firebaseio.com'
 };
